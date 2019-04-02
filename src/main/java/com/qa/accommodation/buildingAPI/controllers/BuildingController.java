f2aea57ae068830f6d2f034679431428437a269b
smurfjames101 <smurfjames@gmail.com>
2019-04-02 17:37:21 +0100

    Fixed Update

--- src/main/java/com/qa/accommodation/buildingAPI/controllers/BuildingController.java
+++ src/main/java/com/qa/accommodation/buildingAPI/controllers/BuildingController.java
@@ -1,11 +1,14 @@
 package com.qa.accommodation.buildingAPI.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,28 +26,33 @@ public class BuildingController {
 		this.buildingService = buildingService;
 	}
 	
-	@PostMapping("/createBuilding")
+	@PostMapping(Constants.CREATE_URL)
 	public String createBuilding(@RequestBody Building building) {
 		return this.buildingService.createBuilding(building);
 	}
 	
-	@GetMapping("/getAllBuildings")
+	@GetMapping(Constants.GET_ALL_URL)
 	public List<Building> getAllBuildings() {
 		return this.buildingService.getAllBuildings();
 	}
 	
-	@GetMapping("/buildingSearch")
-	public List<Building> buildingSearch(String buildingName, String buildingLocation, String ownerName){
-		return this.buildingService.buildingSearch(BuildingBuilder.getBuildingBuilder().buildingName(buildingName)
-				.buildingLocation(buildingLocation).ownerName(ownerName).buildingBuild());
+	@GetMapping(Constants.SEARCH_URL)
+	public List<Building> buildingSearch(String buildingName){
+		return this.buildingService.buildingSearch(BuildingBuilder.getBuildingBuilder().buildingName(buildingName).buildingBuild());
 	}
 	
-	@DeleteMapping("/deleteBuilding")
-	public String deleteBuilding(String buildingName, String buildingLocation, String ownerName) {
-		List<Building> buildings = this.buildingSearch(buildingName, buildingLocation, ownerName);
+	@DeleteMapping(Constants.DELETE_URL)
+	public String deleteBuilding(String buildingName) {
+		List<Building> buildings = this.buildingSearch(buildingName);
 		for(int i = 0; i < buildings.size(); i++) {
 			this.buildingService.deleteBuilding(buildings.get(i));
 		}
 		return Constants.getDeleteSuccess();
+	}
+	@PutMapping(Constants.UPDATE_URL)
+	public String updateBuilding(@PathVariable("buildingName")String buildingName, @RequestBody Building updateBuilding)
+	{
+		return this.buildingService.updateBuilding(buildingName, updateBuilding);
+		
 	}
 }
